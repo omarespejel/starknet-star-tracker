8af325b3bd4993b849b39e3a9446608e9c3eb55c
Omar U. Espejel <espejelomar@gmail.com>
2024-04-13 21:06:48 +0200

    debug in watching directories beneath the current one

--- github-metrics/github_metrics/utils.py
+++ github-metrics/github_metrics/utils.py
@@ -14,7 +14,7 @@ def load_all_developers_dataset():
         # print(os.listdir("../.."))
         # print(os.listdir("./github_metrics"))
 
-        df = pd.read_csv("data/source/all_networks_developer_classification.csv")
+        df = pd.read_csv("../data/source/all_networks_developer_classification.csv")
         df["month_year"] = pd.to_datetime(df["month_year"], format="%B_%Y")
         return df
     except Exception as e:
